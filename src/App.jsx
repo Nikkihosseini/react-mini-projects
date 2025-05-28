567b91edc524436840b111a62d8e67ec27bb9861
Nikkihosseini <niekal00@gmail.com>
2025-05-28 14:22:20 +0330

    create a userCard with props and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './index.css'
 import React from 'react'
+import UserCard from "./Pages/props/UserCard.jsx"
 
 export default function App() {
   return (
     <>
+
+    <UserCard img="https://api.dicebear.com/9.x/adventurer/svg?seed=Easton"
+    name='John Mark'
+    age={22}
+    job='Programing'/>
       
     </>
   )
---
+++ src/Pages/props/UserCard.jsx
@@ -0,0 +1,17 @@
+
+export default function UserCard(props){
+    return(
+        <>
+            <div className="flex items-center justify-center w-screen h-screen bg-slate-800">
+               <div className="bg-stone-200 w-[300px] h-[350px] p-5 rounded-md text-stone-900 text-center shadow-md shadow-stone-100">
+                    <div className="overflow-hidden rounded-full w-[200px] h-[200px] mx-auto">
+                        <img className="inline-block w-full h-full object-cover" src={props.img} alt="user-img"/>
+                    </div>
+                    <h1 className="text-xl font-bold mb-1">{props.name}</h1>
+                    <span className="block mb-1 font-semibold">{props.age}</span>
+                    <span className="block">{props.job}</span>
+               </div>
+            </div>
+        </>
+    )
+}
