0a3e636f3e83ded6ea7dc92422bd722f70bf7604
Nikkihosseini <niekal00@gmail.com>
2025-05-29 16:01:21 +0330

    add show more/less btn for userCard with usestate

--- src/Pages/props/UserCard.jsx
+++ src/Pages/props/UserCard.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react"
 
 export default function UserCard(props){
+    
+    const [showMore , setShowmore] = useState(false)
+
+    const moreDetails = ()=>{
+        setShowmore(!showMore)
+    }
+
     return(
         <>
             <div className="flex items-center justify-center w-screen h-screen bg-slate-800 overflow-x-hidden">
-               <div className="bg-stone-200 w-auto h-[350px] p-5 rounded-md text-stone-900 text-center shadow-md shadow-stone-100">
+               <div className="bg-stone-200 w-[292px] h-auto p-5 rounded-md text-stone-900 text-center shadow-md shadow-stone-100">
                     <div className="overflow-hidden rounded-full w-[200px] h-[200px] mx-auto">
                         <img className="inline-block w-full h-full object-cover" src={props.img} alt="user-img"/>
                     </div>
                     <h1 className="text-xl font-bold mb-1">{props.name}</h1>
                     <span className="block mb-1 font-semibold">{props.age}</span>
-                    <span className="block">{props.job}</span>
+                    <span className="block mb-1">{props.job}</span>
+                    {showMore && <span className="block mb-1">{props.email}</span>}
+                    {showMore && <span className="block">{props.description}</span>}
+                    <button onClick={moreDetails} className="mt-3 px-4 py-1 bg-stone-800 text-white rounded-md hover:bg-stone-700 transition duration-200 focus:outline-none">{!showMore ? 'Show More' : 'Show Less'}</button>
                </div>
             </div>
         </>
--- src/Pages/props/UsersData.jsx
+++ src/Pages/props/UsersData.jsx
@@ -1,7 +1,31 @@
 const users = [
-    {id: 1 , name: 'Sara Lopes' , age: 25 , job: 'Data', img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Aidan'},
-    {id: 2 , name: 'Nick Park' , age: 23 , job: 'Full stack Developer', img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Liam'},
-    {id: 3 , name: 'Adenis' , age: 26 , job: 'Back End Developer', img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Emery'}
+    {
+        id: 1 , 
+        name: 'Sara Lopes' , 
+        age: 25 , 
+        job: 'Data', 
+        img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Aidan', 
+        email: 'SaraLopes@email.com', 
+        description: "Loves working with AI and big data."
+    },
+    {
+        id: 2 , 
+        name: 'Nick Park' , 
+        age: 23 , 
+        job: 'Full stack Developer', 
+        img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Liam', 
+        email: 'NickPark@email.com', 
+        description: "Loves working code"
+    },
+    {
+        id: 3 , 
+        name: 'Adenis' , 
+        age: 26 , 
+        job: 'Back End Developer', 
+        img: 'https://api.dicebear.com/9.x/adventurer/svg?seed=Emery', 
+        email: 'Adenis@email.com', 
+        description: "Loves working with Serves and Data"
+    }
 ]
 
 export default users;
