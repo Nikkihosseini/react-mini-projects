c4894d5fe47fb74a6cf313b711f1fa2b1945562e
Nikkihosseini <niekal00@gmail.com>
2025-05-30 20:51:05 +0330

    feat: add deleteUser button, create Home component, and pass user data via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
+import Home from './Pages/Home'
 import React from 'react'
-import UserCard from "./Pages/props/UserCard.jsx"
-import users from './Pages/props/UsersData.jsx'
 import './index.css'
 
 
 export default function App() {
   return (
     <>
-
-    <div className="flex flex-wrap justify-center items-center gap-8 min-h-screen bg-slate-800 overflow-x-hidden">
-      {
-        users.map(user =>(
-          <UserCard key={user.id} {...user} />
-        ))}
-    </div>
+     <Home/>
     </>
   )
 }
