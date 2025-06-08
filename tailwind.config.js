985d2cf9b4afe8737a4b63b92bed70f42ab11bf9
Nikkihosseini <niekal00@gmail.com>
2025-06-08 20:47:09 +0330

    fead: Add header component to pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
         pixel: ['"Press Start 2P"', 'cursive']
       },
       colors:{
-        "Void-Black": '#0E0E14',
-        "Cosmic-Indigo": '#3A1C71',
-        "Neon-Blue": '#4D9DE0',
-        "Retro-Mint-Green": '#7ED6A7',
-        "Purple-Glitch": '#8F4EAB',
-        "Lime-CRT-Glow": '#A8E6CF',
-        'Foggy-Lilac': '#C8B6FF'
+        "void-black": '#0E0E14',
+        "cosmic-indigo": '#3A1C71',
+        "neon-blue": '#4D9DE0',
+        "retro-mint-green": '#7ED6A7',
+        "purple-glitch": '#8F4EAB',
+        "lime-crt-glow": '#A8E6CF',
+        'foggy-lilac': '#C8B6FF'
       },
     },
   },
