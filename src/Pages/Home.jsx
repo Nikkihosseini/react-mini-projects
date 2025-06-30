cb408e47e95024c75dd8dd310361128cfdcf7979
Nikkihosseini <niekal00@gmail.com>
2025-06-30 23:27:03 +0330

    style: add change them btn to header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,25 +2,30 @@ import {Link} from 'react-router-dom'
 
 
 export default function Home(){
+
     return(
         <> 
             <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-void-black overflow-x-hidden">
                 <ul className='flex items-center flex-col gap-5 text-center'>
-                        <li className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">
-                            <Link to='/'>Home</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/sword.png" alt="sword-png"/>
+                        <li>
+                            <Link to='/'
+                            className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">Home
+                             <img className='inline-block w-7 h-7' src="./public/png-icon/sword.png" alt="sword-png"/></Link> 
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">
-                            <Link to='/Project'>Projects</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/star.png" alt="star-png"/>
+                        <li>
+                            <Link to='/Project'
+                            className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">Projects
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/star.png" alt="star-png"/></Link>
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">
-                            <Link to='/About'>About</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/heart.png" alt="heart-png"/>
+                        <li>
+                            <Link to='/About'
+                            className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">About
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/heart.png" alt="heart-png"/></Link>
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">
-                            <Link to='/Contact'>Contact</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/chat.png" alt="chat-png"/>
+                        <li>
+                            <Link to='/Contact'
+                            className="flex items-center justify-center gap-3 text-purple-600 border-[3px] border-purple-600 bg-black px-5 py-2 shadow-[4px_4px_0px_rgba(255,255,0,0.5)] hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[1px_1px_0px_rgba(255,255,0,0.5)] transition cursor-pointer">Contact
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/chat.png" alt="chat-png"/></Link>
                         </li>
                 </ul>
             </div>    
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom"
+import { useState , useEffect } from "react";
 
 export default function Header(){
+
+    const [isDark, setIsDark] = useState(false);
+
+    const toggleTheme = () => {
+        document.documentElement.classList.toggle("dark");
+        setIsDark(!isDark);
+    };
+
+    useEffect(() => {
+        const darkModeOn = document.documentElement.classList.contains("dark");
+        setIsDark(darkModeOn);
+    }, []);
+
     return(
      <>
         <header className="fixed top-8 right-0 left-0 w-[90%] h-20 z-50 bg-purple-glitch/20 backdrop-blur-md border-4 border-neon-blue p-3 mx-auto">
@@ -9,21 +23,30 @@ export default function Header(){
                 🕹️ Projects Archive
                 </h1>
                 <ul className='flex items-center gap-5 text-center'>
-                        <li className="flex items-center justify-center gap-3 text-lime-crt-glow cursor-pointer">
-                            <Link to='/'>Home</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/sword.png" alt="sword-png"/>
+                        <li>
+                            <Link to='/'
+                            className="flex items-center justify-center gap-3 text-lime-crt-glow hover:text-retro-mint-green cursor-pointer">Home
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/sword.png" alt="sword-png"/></Link>
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-lime-crt-glow cursor-pointer">
-                            <Link to='/Project'>Projects</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/star.png" alt="star-png"/>
+                        <li>
+                            <Link to='/Project'
+                             className="flex items-center justify-center gap-3 text-lime-crt-glow hover:text-retro-mint-green cursor-pointer">Projects
+                             <img className='inline-block w-7 h-7' src="./public/png-icon/star.png" alt="star-png"/></Link>
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-lime-crt-glow cursor-pointer">
-                            <Link to='/About'>About</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/heart.png" alt="heart-png"/>
+                        <li>
+                            <Link to='/About'
+                            className="flex items-center justify-center gap-3 text-lime-crt-glow hover:text-retro-mint-green cursor-pointer">About
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/heart.png" alt="heart-png"/></Link>
                         </li>
-                        <li className="flex items-center justify-center gap-3 text-lime-crt-glow cursor-pointer">
-                            <Link to='/Contact'>Contact</Link>
-                            <img className='inline-block w-7 h-7' src="./public/png-icon/chat.png" alt="chat-png"/>
+                        <li>
+                            <Link to='/Contact'
+                            className="flex items-center justify-center gap-3 text-lime-crt-glow hover:text-retro-mint-green cursor-pointer">Contact
+                            <img className='inline-block w-7 h-7' src="./public/png-icon/chat.png" alt="chat-png"/></Link>
+                        </li>
+                         <li onClick={toggleTheme}
+                                className="flex items-center justify-center gap-3 text-lime-crt-glow hover:text-retro-mint-green cursor-pointer">
+                                Them
+                                {/* <img src={isDark ? "/icons/sun.png" : "/icons/moon.png"} alt="theme-icon" /> */}
                         </li>
                 </ul>
            </div>
