0239d3fc963a4f81a05c53a9c7d2e2b4f744f8d5
Nikkihosseini <niekal00@gmail.com>
2025-07-10 16:07:06 +0330

    style: adjust image sizes, spacing, and visibility toggling

--- src/component/ThemeToggleButton.jsx
+++ src/component/ThemeToggleButton.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect } from "react";
 
-export default function ThemeToggleButton(){
+export default function ThemeToggleButton(props){
 
     const [isDark , setIsDark] = useState(false)
 
@@ -21,7 +21,7 @@ export default function ThemeToggleButton(){
                 className="flex items-center justify-center gap-3 text-pastel-glitch-purple dark:text-lime-crt-glow hover:text-soft-lavender dark:hover:text-retro-mint-green cursor-pointer" 
                 >
                     Theme
-               <img className='inline-block w-6 h-6' src={isDark ? "/png-icon/sun.png" : "/png-icon/moon.png"} alt="theme-icon" />
+               <img className={`${props.isOpen ? "inline-block" : 'hidden'} lg:inline-block w-5 h-5 lg:w-6 lg:h-6 mb-1.5`} src={isDark ? "/png-icon/sun.png" : "/png-icon/moon.png"} alt="theme-icon" />
             </div>
         </>
     )
