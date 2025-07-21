e9a40d2223dff252328326b976605b0c3cbbd7ab
Nikkihosseini <niekal00@gmail.com>
2025-07-21 20:28:30 +0330

    feat: add navigation to cart page on cart icon click in header

--- src/component/PokemonShopHeader.jsx
+++ src/component/PokemonShopHeader.jsx
@@ -1,9 +1,12 @@
 import {useContext } from "react";
 import {CartContext} from "../component/context/CartContext"
+import { useNavigate } from "react-router-dom";
 
 export default function PokemonShopHeader(){
     
     const {cartItems} = useContext(CartContext)
+
+    const navigate = useNavigate();
 
     const totalquantity = cartItems.reduce((sum , item) => sum + item.quantity , 0)
 
@@ -12,7 +15,7 @@ export default function PokemonShopHeader(){
             <div className="flex items-center justify-between w-full border-2 border-neon-blue uppercase text-retro-mint-green p-2">
                 <h1 className="text-sm xs:text-base sm:text-3xl">Pokemon Shop</h1>
                 <div className="flex items-center justify-end w-36 text-left">
-                    <span className="flex items-center text-sm sm:text-base cursor-pointer"><img className="hidden xs:inline-block w-4 h-4 sm:w-6 sm:h-6 mb-2" src="/png-icon/shopping_cart.png" alt="shopping_cart"/>cart:{totalquantity}</span>
+                    <span onClick={()=> navigate("/PokemonShopCart") } className="flex items-center text-sm sm:text-base cursor-pointer"><img className="hidden xs:inline-block w-4 h-4 sm:w-6 sm:h-6 mb-2" src="/png-icon/shopping_cart.png" alt="shopping_cart"/>cart:{totalquantity}</span>
                 </div>
             </div>
         </>
