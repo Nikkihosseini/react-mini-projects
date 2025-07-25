bfdc1351e03eb94e6b369e40afa84ccc6b272e71
Nikkihosseini <niekal00@gmail.com>
2025-07-25 14:20:43 +0330

    fix: adjust layout spacing, sizes, and padding for improved UI consistency

--- src/component/PokemonShopCart.jsx
+++ src/component/PokemonShopCart.jsx
@@ -11,32 +11,30 @@ export default function PokemonShopCart(){
 
     return(
         <>
-            <Header />
-            <div className="container mx-auto">
-                
-                <div className="fixed right-0 left-0 top-32">
-                    <PokemonShopHeader/>
-                </div>
+             <Header />
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between gap-y-8 mt-20 md:mt-32 mb-8">
+          
+              <div className="z-50 w-full">
+                <PokemonShopHeader/>
+              </div>
 
-                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-5">
+              <div className="flex flex-col items-center justify-between bg-transparent backdrop-blur-md border-2 border-neon-blue uppercase text-retro-mint-green p-1 w-full">
 
-                            <div className="flex flex-col items-center justify-between bg-transparent backdrop-blur-md border-2 border-neon-blue uppercase text-retro-mint-green p-1 w-full">
-
-                                <div className="w-full">
-                                    <h2 className="text-2xl text-left w-full line-clamp-1"></h2>
+                            <div className="w-full">
+                                <h2 className="text-2xl text-left w-full line-clamp-1"></h2>
                                 <div className="w-full aspect-square max-w-sm">
                                     <img
                                     className="w-full h-full object-contain bg-retro-mint-green/50"
-                                    
                                     alt="pokemon"
                                     />
                                 </div>
-                                </div>
-              
                             </div>
+              
                         </div>
-
-            </div>
+  
+        </div>
+      </div>
         </>
     )
 }
