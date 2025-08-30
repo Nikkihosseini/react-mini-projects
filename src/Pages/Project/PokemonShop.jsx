c793726fb90f32822f283db091e343f007f28372
Nikkihosseini <niekal00@gmail.com>
2025-08-30 18:02:38 +0330

    fix: remove w-screen from body and apply w-full to main container for proper alignment and scrolling

--- src/Pages/Project/PokemonShop.jsx
+++ src/Pages/Project/PokemonShop.jsx
@@ -56,40 +56,33 @@ function loadPokemons() {
   return (
     <>
       <Header />
-     <div className="w-screen overflow-x-hidden mt-36">
 
-     <div className="fixed right-0 left-0 mx-auto top-20 md:top-36 z-50">
+      <div className="flex items-center justify-center fixed right-0 left-0 top-20 md:top-36 z-50">
        <PokemonShopHeader/>
-     </div>
+      </div>
 
-        <div className="container mx-auto">
-
-            <div className="flex justify-center items-center flex-col gap-5 mt-5 md:mt-20 mx-auto">
-
-              <div className="flex items-center justify-center">
-              
+      
+        <div className="container mt-36 mx-auto">
+          <div className="flex justify-center items-center flex-col gap-y-5 mt-5 md:mt-20">
+           
               <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-10">
                 {pokemonList.map(poke => (
-                  <PokemonBox key={poke.id} poke={poke} />
+                  <PokemonBox key={poke.id} poke={poke}/>
                 ))}
               </div>
-      
-            </div>
-            <div className="flex justify-center mb-5">
-                <button
+
+              <div className="flex justify-center mb-5">
+                  <button
                 onClick={loadPokemons}
                 disabled={loading}
                 className="px-4 py-2 text-retro-mint-green bg-neon-blue/10 hover:bg-neon-blue/20 transition-all outline-none border-none focus:outline-none"
                 >
                 {loading ? "Loading..." : "Show More"}
-                </button>
-            </div>
+                  </button>
+              </div>
 
-            </div>
-    
+          </div>
         </div>
-
-     </div>
     </>
   );
 }
