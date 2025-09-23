39514166c67877b8ea2753a33d7749179ff28c03
Nikkihosseini <niekal00@gmail.com>
2025-09-23 16:28:15 +0330

    style: make About component responsive

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -14,18 +14,19 @@ export default function About() {
             <Footer/>
         </div>
                
-        <div className="flex flex-col items-center justify-center  min-h-screen text-center p-6">
-            <h1 className="text-4xl font-bold mb-6">About This Project !</h1>
-             <p className="max-w-xl text-lg leading-relaxed mb-6">
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-3 text-pastel-glitch-purple">
+            <h1 className="text-sm sm:text-xl md:text-4xl font-bold mb-6 w-full">About This Project!</h1>
+            <span className='border-2 border-dashed w-full border-vintage-rose mb-4'></span>
+             <p className="max-w-xl text-xs md:text-lg leading-relaxed mb-6">
                 This project is a pixel-style Pokémon gallery built to practice React and API integration.  
-                It uses <span className="font-bold">React, TailwindCSS, and PokeAPI</span> to fetch and display data,
+                It uses <span className="text-retro-mint-green">React, TailwindCSS, and PokeAPI</span> to fetch and display data,
                 with a nostalgic, retro look.
             </p>
-            <ul className="bg-neon-blue/10 p-4 rounded-2xl text-left shadow-md space-y-2">
-                <li>✅ Practicing React Hooks</li>
-                <li>✅ Fetching data from an API</li>
-                <li>✅ Styling with TailwindCSS</li>
-                <li>✅ Fully responsive design</li>
+            <ul className="felx items-center justify-center bg-neon-blue/10 p-6 pr-3 rounded-2xl text-left shadow-md space-y-2 text-[10px] md:text-base">
+                <li>- Practicing React Hooks</li>
+                <li>- Fetching data from an API</li>
+                <li>- Styling with TailwindCSS</li>
+                <li>- Fully responsive design</li>
             </ul>
         </div>
     </div>
