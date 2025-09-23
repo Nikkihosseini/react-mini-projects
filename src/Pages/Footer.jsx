c1847ba1051c14edf33db0889db211088f18ab2d
Nikkihosseini <niekal00@gmail.com>
2025-09-23 15:49:58 +0330

    style: make Footer component responsive

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="text-lightBlue-550 dark:text-darkBlue-100 text-sm mt-10">
-      <p className="flex items-center gap-1.5">
+    <footer className="text-lightBlue-550 dark:text-darkBlue-100 text-xs md:text-sm mt-10">
+      <p className="flex items-center flex-wrap justify-center gap-1.5">
         Copyright © {new Date().getFullYear()}
         <a
           className="flex items-center gap-1.5 ml-1"
